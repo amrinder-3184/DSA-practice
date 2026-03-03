c0babe00e54c8ec6e76ce3059717bd1c638730a2
Amrinder Singh <108217788+amrinder-3184@users.noreply.github.com>
2026-03-03 13:10:55 +0530

    Binary Tree level order traversal

---
+++ Trees/level_order_traversal.cpp
@@ -0,0 +1,35 @@
+// LeetCode 102 Binary Tree Level Order Traversal
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+
+        if(root == NULL) return {};
+
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+
+        q.push(root);
+        while(!q.empty()){
+
+            int size= q.size();
+            vector<int> level;
+
+            for(int i=0;i<size;i++) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            level.push_back(node->val);
+
+            if(node->left) {
+                q.push(node->left);
+            }
+            if(node->right) {
+                q.push(node->right);
+            }
+        }
+        ans.push_back(level);
+    }
+    return ans;
+    }
+};
