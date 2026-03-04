e2b0dfb233e55d0d4a21baeb66c799366de02834
Amrinder Singh <108217788+amrinder-3184@users.noreply.github.com>
2026-03-04 13:16:59 +0530

    solved LC236_Lowest_Common_Ancestor_of_a_Binary_Tree

---
+++ Trees/LC236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -0,0 +1,17 @@
+// LC236 Lowest Common Ancestor of a Binary Tree
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)return NULL;
+
+        if(root ==p || root ==q)return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        
+        if(left && right) return root;
+
+        return left?left:right;
+    }
+};
